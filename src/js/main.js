6e6d14a2a5dd8cac955384207bac0e8b3167e206
Dmitriy <dima-o@mail.ru>
2022-03-11 23:51:49 +0300

    add js smooth scroll

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+const anchors = document.querySelectorAll('.header__link, .footer__link');
+const headerNav = document.querySelectorAll('.header__item');
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    if(anchor.classList.contains('header__link')) {
+        headerNav.forEach(element => {
+            element.classList.remove('active');
+        });
+        anchor.parentElement.classList.add('active');
+    };
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
+    if(goto !== '#') {
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+  });
+};
